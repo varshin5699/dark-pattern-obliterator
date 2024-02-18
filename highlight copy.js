b451b1bdc3605f3b382b846618eae3060a8930e2
varshin5699 <136289580+varshin5699@users.noreply.github.com>
2024-02-18 10:37:37 +0530

    Delete highlight copy.js

--- highlight copy.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('highButton').addEventListener('click', function() {
-    // Send message to content script
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "highlight"});
-    });
-  });
-
-
